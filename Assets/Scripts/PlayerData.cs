facd7778bd035624647cc056581343ffc6707b7c
AZEUS\rafha <rafhall@hotmail.com>
2024-09-16 19:48:01 +0100

    race selection & player data scripts

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData
+{
+    public static string selectedRace;
+    public static int[] attributes = new int[7]; //strength, dex, end, fai, chr, int, lck
+
+}
+
--- Assets/Scripts/RaceSelectionManager.cs
+++ Assets/Scripts/RaceSelectionManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.RestService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RaceSelectionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public TextMeshProUGUI raceDesc;
+    private string selectedRace;
+
+    public void SelectRace(string race)
     {
-        
+        selectedRace = race;
+        raceDesc.text = GetRaceDescription(race);
     }
 
-    // Update is called once per frame
-    void Update()
+    private string GetRaceDescription(string race)
     {
-        
+        switch (race)
+        {
+            case "Human": return "Humans, typically well known for their versatility and excellent communication.";
+            case "Aldr": return "The Aldr are a highly respected race possessing ancient wisdom.";
+            case "Veld": return "Veld skirmishers have access to the tech of tomorrow and prefer to work in solitude.";
+            default: return "";
+        }
+    }
+
+    public void OnAdvanceButton()
+    {
+        PlayerData.selectedRace = selectedRace;
+        SceneManager.LoadScene("AttributeDistribution");
     }
 }
